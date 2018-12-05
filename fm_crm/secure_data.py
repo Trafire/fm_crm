46685e7505b68ff9093e250e9bc536973368e052
Antoine Wood <antoinewood@gmail.com>
2018-12-04 20:27:18 -0500

    created test template

---
+++ fm_crm/secure_data.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fm_app',
+        'USER': 'po_antoine',
+        'PASSWORD': '$pl4nt3n',
+        'HOST': 'mysql.fleurametztoronto.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
